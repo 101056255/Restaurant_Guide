72a05de3088b44539d43ba2e1dcda7975031fb27
101056255 <edwardphilip30@gmail.com>
2020-11-21 18:55:25 -0500

    updated RestaurantPageActivity for further integration

--- app/src/main/java/ca/gbc/comp3074/restaurantguide/RestaurantPageActivity.java
+++ app/src/main/java/ca/gbc/comp3074/restaurantguide/RestaurantPageActivity.java
@@ -1,14 +1,21 @@
 package ca.gbc.comp3074.restaurantguide;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class RestaurantPageActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restuarant_page);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 }
