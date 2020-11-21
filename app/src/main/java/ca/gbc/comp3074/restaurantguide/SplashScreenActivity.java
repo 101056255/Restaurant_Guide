4254ee72b2dc6742bbb43d826877ad4348cd835e
Edward Philip <Edward Philip@EDWARD-PREDATOR>
2020-11-20 20:04:38 -0500

    added RestaurantPageActivity

--- app/src/main/java/ca/gbc/comp3074/restaurantguide/SplashScreenActivity.java
+++ app/src/main/java/ca/gbc/comp3074/restaurantguide/SplashScreenActivity.java
@@ -5,15 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
 
         final Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
 
@@ -21,6 +24,7 @@ public class SplashScreenActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(intent);
+                finish();
             }
         }, 3000);
     }
